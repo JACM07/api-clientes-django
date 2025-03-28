987ab804592328594e0b75058661f37553c2aa85
JACM07 <joseantoniocastromariscal19@gmail.com>
2025-03-28 09:56:46 -0600

    se cambio la base de datos a MySQL

--- ApiClientesProyecto/ApiClientesProyecto/settings.py
+++ ApiClientesProyecto/ApiClientesProyecto/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'ApiClientesProyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'registroClientes',
+        'HOST': '127.0.0.1',
+        'USER':'root',
+        'PASSWORD':'root1234',
+        'PORT':'3307'
     }
 }
 
--- ApiClientesProyecto/ClientesApp/models.py
+++ ApiClientesProyecto/ClientesApp/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-class Cliente(models.Model):
+class Cliente(models.Model):#bd:registroClientes, tabla:clientesapp_cliente
     idCliente=models.IntegerField(primary_key=True)#auto_created?
     nombre=models.CharField(max_length=250)#null=False
     correo=models.CharField(max_length=100)
     numeroTelefono=models.CharField(max_length=100)
-    edad=models.IntegerField()
+    edadc=models.IntegerField()
