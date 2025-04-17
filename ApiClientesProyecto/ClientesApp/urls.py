52d2adc764456c993ea704a62cdafa3694c77028
JACM07 <joseantoniocastromariscal19@gmail.com>
2025-04-17 14:30:41 -0600

    se agregaron los metodos GET,PUT,DELETE para un solo elemento

--- ApiClientesProyecto/ClientesApp/urls.py
+++ ApiClientesProyecto/ClientesApp/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns=[
     path('',views.registroClientes),
     path('clientes/',views.clientes),
+    path('clientes/<int:idCliente>/', views.cliente),
 ]
 
--- ApiClientesProyecto/ClientesApp/views.py
+++ ApiClientesProyecto/ClientesApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.db import IntegrityError
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseNotAllowed
 from .models import Cliente
 from django.views.decorators.csrf import csrf_exempt
 from django.forms.models import model_to_dict
@@ -36,9 +36,40 @@ def clientes(request):
             return JsonResponse({'error': 'true', 'message': 'falta el campo requerido'}, status=400)
 
 
+
+
+#GET un cliente= recuperar cliente por id
+@csrf_exempt
+def cliente(request, idCliente):  #'clientes/<int:idCliente>/'   PONER '/' AL FINAL PARA QUE FUNCIONE
+    try:
+        cliente=Cliente.objects.get(idCliente=idCliente)    
+    except Cliente.DoesNotExist:
+            return JsonResponse({'error': 'Cliente no encontrado'}, status=404)
+    
+    if request.method == 'GET':
+        return JsonResponse(model_to_dict(cliente))
+        
+    elif request.method == 'PUT':
+        try:
+            data = json.loads(request.body)  #Se lee el JSON
+            cliente.nombre = data.get('nombre',cliente.nombre)
+            cliente.correo = data.get('correo',cliente.correo)
+            cliente.numeroTelefono = data.get('numeroTelefono',cliente.numeroTelefono)
+            cliente.edad = data.get('edad',cliente.edad)
+            cliente.save()
+            return JsonResponse(model_to_dict(cliente), status=200)
+        except Exception as e:
+            return JsonResponse({'error': 'Error al actualizar', 'detalle': str(e)}, status=400)
+    
+    elif request.method == 'DELETE':
+        cliente.delete()
+        return JsonResponse({'message': 'Cliente eliminado'}, status=204)
+
+    else:
+        return HttpResponseNotAllowed(['GET', 'PUT', 'DELETE'])
+
+
+
+
 #REGISTRAR VISTAS NUEVAS DE ABAJO
-
-#GET un cliente
-
-
-#PUT,DELETE un cliente
+#PATCH? = partial_update un cliente
